0d094905843a3ebf823250515eb123f24ac5e6a5
kaji-sysi <kaji@system-i.com>
2025-07-02 14:05:49 +0900

    コメント修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: '残材管理システム',
-  description: '建設現場の残材管理のためのシステム',
+  description: '製造現場の残材管理のためのシステム',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><rect width="32" height="32" fill="%232563eb"/><path d="M8 8h16v4H8zm0 6h12v4H8zm0 6h16v4H8z" fill="white"/></svg>',
   },
